f99317ecd04a2fd3e0baa112f9c32d8312f2d927
dessarion <dessuas@gmial.com>
2020-08-13 17:05:01 +0300

    finished homework #10

--- JSC_tenth_hw/app.js
+++ JSC_tenth_hw/app.js
@@ -30,31 +30,52 @@ setInterval(currentTime, 1000)
 
 function stopWatch() {
 
-    let time
-    let start 
-    let mark
-    let corr
+    let time, start, mark, corr
 
-    function log(params) {
-        console.log(params);
+    function stringRender(miliseconds) {
+
+        let hh, mm, ss, ms
+
+        ms = miliseconds % 1000
+        if (ms <= 9) {
+            ms = '00' + ms
+        } else if (ms <= 99) {
+            ms = '0' + ms
+        }
+
+        ss = Math.floor((miliseconds % (60000)) / 1000)
+        if (ss <= 9) {
+            ss = '0' + ss
+        }
+
+        mm = Math.floor((miliseconds % (3600000)) / 60000)
+        if (mm <= 9) {
+            mm = '0' + mm
+        }
+
+        hh = Math.floor((miliseconds % (86400000)) / 3600000)
+        if (hh <= 9) {
+            hh = '0' + hh
+        }
+
+        return `${hh}:${mm}:${ss}:<span class="seconds">${ms}</span>`
+    }
+
+    function lapList(miliseconds) {
+        let listItem = document.createElement('li')
+        listItem.innerHTML = stringRender(miliseconds)
+        $('.stopwatch__laps--list').append(listItem)
     }
 
 
     $('[data-btn = "start"]').addEventListener('click', () => {
-        
-        // if(start === undefined){
-            start =  Date.now()            
-            console.log(start,'plc');
-        // } else {
-            
-        // }
+
+        start = Date.now()
 
         let int = setInterval(() => {
             time = Date.now() - start + (corr === undefined ? 0 : corr)
-            
-            log(time)
-        }, 1000);
-
+            $('.stopwatch__display--dights').innerHTML = stringRender(time)
+        }, 1);
 
         $('[data-btn = "stop"]').addEventListener('click', () => {
             mark = Date.now()
@@ -62,15 +83,100 @@ function stopWatch() {
             clearInterval(int)
         })
 
+        $('[data-btn = "reset"]').addEventListener('click', () => {
+            corr = 0
+            time = 0
+            start = 0
+            clearInterval(int)
+            $('.stopwatch__display--dights').innerHTML = stringRender(time)
+            $('.stopwatch__display--dights span').classList.remove('seconds')
+            $('.stopwatch__laps--list').innerHTML = ''
+        })
+    })
 
+    $('[data-btn = "lap"]').addEventListener('click', () => {
+        if ($('.stopwatch__laps--list').children.length < 8) {
+            lapList(time)
+        } else {
+            $('.stopwatch__laps--list').innerHTML = ''
+            lapList(time)
+        }
     })
 }
-   
 stopWatch()
 
+function timer() {
 
+    const conditionDisplay = $('.timer__set--display_dights')
+    const countDownDisplay = $('.timer__countdown--display')
 
+    let condition = +(conditionDisplay.textContent)
 
+    let time, conds
 
+    function setTimeZero(minutes, seconds) {
+        let timeZero = new Date()
+        let currentMinutes = timeZero.getMinutes()
+        let temp = currentMinutes + minutes
+        timeZero.setMinutes(temp)
+        return timeZero.getTime() + 1000 + (seconds === undefined ? 0 : seconds * 1000)
+    }
 
+    function renderIterator(miliseconds) {
+        let ss = Math.floor((miliseconds % (60000)) / 1000)
+        if (ss <= 9) {
+            ss = '0' + ss
+        }
 
+        let mm = Math.floor((miliseconds % (3600000)) / 60000)
+        if (mm <= 9) {
+            mm = '0' + mm
+        }
+
+        return `${mm}:${ss}`
+    }
+
+    function parserHandler(str) {
+        let mm, ss
+        let temp = []
+        temp = str.split(':')
+        mm = +(temp[0])
+        ss = +(temp[1])
+        return {
+            mm: mm,
+            ss: ss
+        }
+    }
+
+    $('[data-btn="add"]').addEventListener('click', () => {
+        conditionDisplay.textContent = ++condition
+    })
+
+    $('[data-btn="subtract"]').addEventListener('click', () => {
+        conditionDisplay.textContent = --condition
+    })
+
+    $('[data-btn="start_timer"]').addEventListener('click', () => {
+        let buffer = setTimeZero(condition, conds)
+        countDownDisplay.textContent = `${condition}:${conds ? conds : '00'}`
+        let iter = setInterval(() => {
+            time = buffer - Date.now()
+            countDownDisplay.textContent = renderIterator(time)
+        }, 1000);
+
+        $('[data-btn="stop_timer"]').addEventListener('click', () => {
+            condition = parserHandler(countDownDisplay.textContent).mm
+            conds = parserHandler(countDownDisplay.textContent).ss
+            clearInterval(iter)
+        })
+
+        $('[data-btn="reset_timer"]').addEventListener('click',()=>{
+            clearInterval(iter)
+            
+            conditionDisplay.textContent = '25'
+            countDownDisplay.textContent = '00:00'
+            condition = +(conditionDisplay.textContent)
+        })
+    })
+}
+timer()
